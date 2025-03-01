b9ead5146eec3d6c6130d14ab26ae3884d61045b
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-03-01 21:38:12 +0300

    adding last futures to checklist and styling

--- backend/checklist.js
+++ backend/checklist.js
@@ -12,11 +12,24 @@ clicked.addEventListener('click', () =>
     newTask.classList.add('newTask');
     newTask.textContent = task;
 
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('list-checkbox');
+
+    checkBox.addEventListener('change', () => {
+      if (checkBox.checked) {
+        newTask.classList.add('completed');
+      } else {
+        newTask.classList.remove,('completed');
+      }
+    });
+
+    newTask.prepend(checkBox); 
+
     let deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-task-button');
     deleteButton.textContent = 'Delete';
 
-    // Add an event listener to the delete button
     deleteButton.addEventListener('click', () => {
       newTask.remove(); // Removes the task when the delete button is clicked
     });
